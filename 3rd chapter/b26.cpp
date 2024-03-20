24351133fb01f1075dba4c984da562593b6edba4
Amonullo <amonullo436@gmail.com>
2024-03-20 22:16:52 +0200

    add

---
+++ 3rd chapter/b26.cpp
@@ -0,0 +1,10 @@
+/*Given two real numbers x, y, verify the following proposition: ―The
+point with coordinates (x, y) is in the fourth coordinate quarter.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x, y;
+    cin>>x>>y;
+    bool c = (x > 0) && (y < 0);
+    cout<<c;
+}
