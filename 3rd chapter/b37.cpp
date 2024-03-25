5050e7936b527a2ce3f033a8ced7c8a4c2f5cada
Amonullo <amonullo436@gmail.com>
2024-03-25 23:21:41 +0200

    add

---
+++ 3rd chapter/b37.cpp
@@ -0,0 +1,12 @@
+/*Given coordinates x1, y1, x2, y2 of two chessboard squares (as integers in
+the range 1 to 8), verify the following proposition: ―A king can move from one
+square to another during one turn.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int x1, y1, x2, y2;
+    cin>>x1>>y1>>x2>>y2;
+
+    bool a = (x1 == x2 && y1 != y2) || (x1 != x2 && y1 == y2) || (x1 == y1 && x2 == y2);
+    cout<<a;
+}
