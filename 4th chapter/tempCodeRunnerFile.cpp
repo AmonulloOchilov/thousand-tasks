310d01275c773cc5ad56310157804f11ddfdf087
Amonullo <amonullo436@gmail.com>
2024-03-31 00:36:05 +0200

    add

---
+++ 4th chapter/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+/*An integer is given. If the integer is positive then increase it by 1, otherwise do
+not change it. Output the obtained integer.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    
+    if (a>0)
+    {
+        cout<<a+1;
+    }
+    else {
+        cout<<a;
+    }
+}
